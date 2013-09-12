0c70f4587d242bf10986926250496714a941ee2f
msalese <massimo.salese@gmail.com>
2013-09-12 16:21:23 +0200

    added function to watch index and underlying t3GetIndexWatchList t3GetUnderWatchList added function to setup and watch options chain t3SetUpPutChain t3SetUpCallChain t3WatchOptionChain added function to copunte imp vol and greeks on t3opne option chain t3EPutGreeksChain t3ECallGreeksChain

--- T3OpenInterface/T3OpenInterface.m
+++ T3OpenInterface/T3OpenInterface.m
@@ -102,6 +102,55 @@ t3OpenSubscribeOrderBook::usage = "t3OpenSubscribeOrderBook[t3OpenJavaObj] funct
 subscribe a T3Open java object to order book";
 
 
+t3GetIndexWatchList::usage = "t3GetIndexWatchList[idxList] function
+return a whach list for index.
+idxList is a list of index that you want follow: {{'MI','EQCON','I_SPMIB','FTSEMIB'}}";
+
+
+t3GetUnderWatchList::usage = "t3GetUnderWatchList[underList] function
+return a T3Open whatch list for stocks/bonds all instruments with lastPrice, BestBid and BestAsk.
+underList is a list that you want follow : {{'MI','EQCON','1','G'},{'MI','EQCON','81658','TRN'},{'MI','DER','720541','FIB3I'}}";
+
+
+t3SetUpPutChain::usage = "t3SetUpPutChain[exchange,market,name,expMonth,lowStrike,highStrike,stripChar] function
+return a put chain. Parameters:
+exchange es 'MI'
+market es 'DER'
+name es 'FTSE*MIB', you must check it using t3OpneGetTickerByName
+expMonth es '20/09/2013'
+lowStrike es 17500
+highStrike es 16500
+stripChar es 6, the number of char to strip from symbol to exstract strike price (MIBO3U17600)";
+
+
+t3SetUpCallChain::usage = "t3SetUpCallChain[exchange,market,name,expMonth,lowStrike,highStrike,stripChar] function
+return a call chain. Parameters:
+exchange es 'MI'
+market es 'DER'
+name es 'FTSE*MIB', you must check it using t3OpneGetTickerByName
+expMonth es '20/09/2013'
+lowStrike es 17500
+highStrike es 16500
+stripChar es 6, the number of char to strip from symbol to exstract strike price (MIBO3U17600)";
+
+
+t3WatchOptionChain::usage = "t3WatchOptionChain[optionChain] function
+return the T3Open option chain (put/call).
+optionCahin is a list created by t3SetUpCallChain or t3SetUpPutChain.";
+
+
+t3EPutGreeksChain::usage = "t3EPutGreeksChain[putChain,interestRate,currentPrice,dividend] function
+use internal Mathematica function to compute European put implied volatility and greeks from the put chain.
+putChain is build using t3GetPutChain function.
+Implied Vol is computed using middle price between ask and bid";
+
+
+t3ECallGreeksChain::usage = "t3ECallGreeksChain[callChain,interestRate,currentPrice,dividend] function
+use internal Mathematica function to compute European call implied volatility and greeks form the call chain.
+callChain is build using t3GetCallChain function.
+Implied Vol is computed using middle price between ask and bid";
+
+
 Begin["Private`"];
 
 
@@ -436,6 +485,214 @@ myT3OpenObj@getRefOutStream[]@println[request];
 response = myT3OpenObj@getRefBufReader[]@readLine[];
 (*response = Take[Flatten[StringSplit[response,"|"]],-1];*)
 Return[response];
+];
+
+
+t3GetIndexWatchList[idxList_]:=Module[{data,data02Lastprice,subData,lastPrice,lastPrice01,lastPrice02,lastUpdate,finalPutChain,finalPutChain01 },
+data = idxList;
+data02Lastprice = Table[t3OpenObject[{data[[i]]}],{i,1,Length[data]}];
+subData =Table[Append[data[[i]],data02Lastprice[[i,1]][[5]]],{i,1,Length[data]}];
+(*first call to subscribed data*)
+Table[t3OpenSubscribeLastPrice[subData[[i,5]],subData[[i,1]],subData[[i,2]],subData[[i,3]]],{i,1,Length[data]}];
+(*get subscribed data*)
+lastPrice =Table[t3OpenGetSubscribedData[subData[[i,5]]],{i,1,Length[data]}];
+(*here we should check if lastPrice is null, if null you have to request again*)
+If[MemberQ[lastPrice,{Null}],Return[1];Abort[]];
+(*format for better output*)
+lastPrice01 = Table[StringSplit[lastPrice[[i]],"|"],{i,1,Length[data]}];
+lastPrice02=lastPrice01[[All,1]][[All,2]];
+lastUpdate = Table[DateString[],{Length[data]}];
+finalPutChain={lastUpdate,data[[All,3]],lastPrice02};
+finalPutChain01 = Prepend[Transpose[finalPutChain],{"LastUpdateTime","Symbol","LastPrice"}];
+(*destroy java objects*)
+Table[t3OpenUnsubscribe[subData[[i,5]]],{i,1,Length[data]}];
+Return[finalPutChain01];
+];
+
+
+t3GetUnderWatchList[underList_]:=Module[{data,data02Lastprice,data02BestBid1 ,data02BestAsk1,subData,lastPrice,bestBid1,bestAsk1,lastPrice01,bestBid101,bestAsk101,lastPrice02,bestBid102,bestAsk102,lastUpdate,finalPutChain,finalPutChain01 },
+data = underList;
+data02Lastprice = Table[t3OpenObject[{data[[i]]}],{i,1,Length[data]}];
+data02BestBid1 =Table[t3OpenObject[{data[[i]]}],{i,1,Length[data]}];
+data02BestAsk1 =Table[t3OpenObject[{data[[i]]}],{i,1,Length[data]}];
+subData =Table[Append[Append[Append[data[[i]],data02Lastprice[[i,1]][[5]]],data02BestBid1[[i,1]][[5]]],data02BestAsk1[[i,1]][[5]]],{i,1,Length[data]}];
+(*first call to subscribed data*)
+Table[t3OpenSubscribeLastPrice[subData[[i,5]],subData[[i,1]],subData[[i,2]],subData[[i,3]]],{i,1,Length[data]}];
+Table[t3OpenSubscribeBestBid1[subData[[i,6]],subData[[i,1]],subData[[i,2]],subData[[i,3]]],{i,1,Length[data]}];
+Table[t3OpenSubscribeBestAsk1[subData[[i,7]],subData[[i,1]],subData[[i,2]],subData[[i,3]]],{i,1,Length[data]}];
+(*get subscribed data*)
+lastPrice =Table[t3OpenGetSubscribedData[subData[[i,5]]],{i,1,Length[data]}];
+bestBid1 =Table[t3OpenGetSubscribedData[subData[[i,6]]],{i,1,Length[data]}];
+bestAsk1=Table[t3OpenGetSubscribedData[subData[[i,7]]],{i,1,Length[data]}];
+(*here we should check if lastPrice is null, if null you have to request again*)
+If[MemberQ[lastPrice,{Null}],Return[1];Abort[]];
+If[MemberQ[bestBid1,{Null}],Return[1];Abort[]];
+If[MemberQ[bestAsk1,{Null}],Return[1];Abort[]];
+(*format for better output*)
+lastPrice01 = Table[StringSplit[lastPrice[[i]],"|"],{i,1,Length[data]}];
+bestBid101 = Table[StringSplit[bestBid1[[i]],"|"],{i,1,Length[data]}];
+bestAsk101 = Table[StringSplit[bestAsk1[[i]],"|"],{i,1,Length[data]}];
+lastPrice02=lastPrice01[[All,1]][[All,2]];
+bestBid102= bestBid101[[All,1]][[All,2]];
+bestAsk102 = bestAsk101[[All,1]][[All,2]];
+lastUpdate = Table[DateString[],{Length[data]}];
+finalPutChain={lastUpdate,data[[All,4]],lastPrice02,bestBid102,bestAsk102};
+finalPutChain01 = Prepend[Transpose[finalPutChain],{"LastUpdateTime","Symbol","LastPrice","BestBid1","BestAsk1"}];
+(*destroy java objects*)
+Table[t3OpenUnsubscribe[subData[[i,5]]],{i,1,Length[data]}];
+Table[t3OpenUnsubscribe[subData[[i,6]]],{i,1,Length[data]}];
+Table[t3OpenUnsubscribe[subData[[i,7]]],{i,1,Length[data]}];
+Return[finalPutChain01];
+];
+
+
+t3SetUpPutChain[exchange_String,market_String,name_String,expMonth_String,lowStrike_,highStrike_,stripChar_]:=Module[
+{pattern,searchList,searchList02,outcome,searchList03,searchList04,code,symbol,strike,data,exch,mrkt,expDate },
+pattern = StringJoin["OPTION*",name,"*",expMonth,"*","PUT"];
+searchList = t3OpenGetTickerByName[exchange,market,pattern];
+searchList02 = StringSplit[searchList,{"outcome=","numElements=",{"element="}}];
+outcome =StringDrop[searchList02[[1]],-1];
+searchList03 = Drop[searchList02,2];
+searchList04 =StringSplit[searchList03,"|"];
+code = searchList04 [[All,3]];
+symbol = StringTrim[searchList04 [[All,5]]];
+strike = ToExpression[Flatten[StringSplit[symbol,Take[StringTake[symbol,stripChar],1]]]];
+expDate = Table[expMonth,{Length[code]}];
+exch = searchList04[[All,1]];
+mrkt = searchList04[[All,2]];
+data = Transpose[{code,symbol,strike,expDate,exch,mrkt}];
+data = Sort[data,data[[All,3]]];
+data =Select[data,(#[[3]] <= highStrike && #[[3]]>=lowStrike )&];
+data = Prepend[data,{"Code","Symbol","Strike","ExpDate","Exchange","Market"}];
+Return[data];
+]
+
+
+t3SetUpCallChain[exchange_String,market_String,name_String,expMonth_String,lowStrike_,highStrike_,stripChar_]:=Module[
+{pattern,searchList,searchList02,outcome,searchList03,searchList04,code,symbol,strike,data,exch,mrkt,expDate },
+pattern = StringJoin["OPTION*",name,"*",expMonth,"*","CALL"];
+searchList = t3OpenGetTickerByName[exchange,market,pattern];
+searchList02 = StringSplit[searchList,{"outcome=","numElements=",{"element="}}];
+outcome =StringDrop[searchList02[[1]],-1];
+searchList03 = Drop[searchList02,2];
+searchList04 =StringSplit[searchList03,"|"];
+code = searchList04 [[All,3]];
+symbol = StringTrim[searchList04 [[All,5]]];
+strike = ToExpression[Flatten[StringSplit[symbol,Take[StringTake[symbol,stripChar],1]]]];
+expDate = Table[expMonth,{Length[code]}];
+exch = searchList04[[All,1]];
+mrkt = searchList04[[All,2]];
+data = Transpose[{code,symbol,strike,expDate,exch,mrkt}];
+data = Sort[data,data[[All,3]]];
+data =Select[data,(#[[3]] <= highStrike && #[[3]]>=lowStrike )&];
+data = Prepend[data,{"Code","Symbol","Strike","ExpDate","Exchange","Market"}];
+Return[data];
+]
+
+
+t3WatchOptionChain[optionChain_List]:=Module[
+{data,data02Lastprice,data02BestBid1 ,data02BestAsk1,subData,lastPrice,bestBid1,bestAsk1,lastPrice01,bestBid101,
+bestAsk101,lastPrice02,bestBid102,bestAsk102,lastUpdate,finalPutChain,finalPutChain01,daysToExp,daysToExpY},
+data = Drop[optionChain,1];
+(*now we build java object for last_price, best_bid1 and best_ask1*)
+data02Lastprice = Table[t3OpenObject[{data[[i]]}],{i,1,Length[data]}];
+data02BestBid1 =Table[t3OpenObject[{data[[i]]}],{i,1,Length[data]}];
+data02BestAsk1 =Table[t3OpenObject[{data[[i]]}],{i,1,Length[data]}];
+subData =Table[Append[Append[Append[data[[i]],data02Lastprice[[i,1]][[7]]],data02BestBid1[[i,1]][[7]]],data02BestAsk1[[i,1]][[7]]],{i,1,Length[data]}];
+(*first call to subscribed data*)
+Table[t3OpenSubscribeLastPrice[subData[[i,7]],data[[i,5]],data[[i,6]],subData[[i,1]]],{i,1,Length[data]}];
+Table[t3OpenSubscribeBestBid1[subData[[i,8]],data[[i,5]],data[[i,6]],subData[[i,1]]],{i,1,Length[data]}];
+Table[t3OpenSubscribeBestAsk1[subData[[i,9]],data[[i,5]],data[[i,6]],subData[[i,1]]],{i,1,Length[data]}];
+(*get subscribed data*)
+lastPrice = Table[t3OpenGetSubscribedData[subData[[i,7]]],{i,1,Length[data]}];
+bestBid1 = Table[t3OpenGetSubscribedData[subData[[i,8]]],{i,1,Length[data]}];
+bestAsk1 = Table[t3OpenGetSubscribedData[subData[[i,9]]],{i,1,Length[data]}];
+(*here we should check if lastPrice is null, if null you have to request again*)
+If[MemberQ[lastPrice,{Null}],Return[1];Abort[]];
+If[MemberQ[bestBid1,{Null}],Return[1];Abort[]];
+If[MemberQ[bestAsk1,{Null}],Return[1];Abort[]];
+(*format for better output*)
+lastPrice01 = Table[StringSplit[lastPrice[[i]],"|"],{i,1,Length[data]}];
+bestBid101 = Table[StringSplit[bestBid1[[i]],"|"],{i,1,Length[data]}];
+bestAsk101 = Table[StringSplit[bestAsk1[[i]],"|"],{i,1,Length[data]}];
+lastPrice02 = lastPrice01[[All,1]][[All,2]];
+bestBid102 = bestBid101[[All,1]][[All,2]];
+bestAsk102 = bestAsk101[[All,1]][[All,2]];
+lastUpdate = Table[DateString[{"Day","/","Month","/","Year"," ","Hour24",":","Minute",":","Second"}],{Length[data]}];
+(*prepare for output*)
+daysToExp =Table[ DateDifference[Take[DateList[],3],Take[DateList[data[[1,4]]],3]],{Length[data]}];
+daysToExpY = N[daysToExp/365];
+finalPutChain={lastUpdate,data[[All,2]],lastPrice02,bestBid102,bestAsk102,data[[All,3]],daysToExp,daysToExpY};
+finalPutChain01 = Prepend[Transpose[finalPutChain],{"LastUpdateTime","Symbol","LastPrice","BestBid1","BestAsk1","Strike","DToExp","DToExpY"}];
+(*destroy java objects*)
+Table[t3OpenUnsubscribe[subData[[i,4]]],{i,1,Length[data]}];
+Table[t3OpenUnsubscribe[subData[[i,5]]],{i,1,Length[data]}];
+Table[t3OpenUnsubscribe[subData[[i,6]]],{i,1,Length[data]}];
+Return[finalPutChain01];
+];
+
+
+t3EPutGreeksChain[chain_,interestRate_,currentPrice_,dividend_]:=Module[
+{chain01,chain02,chain03,chain04,chain05,chain06,chain07,chain08,chainIvf},
+(*remove column name*)
+chain01=Drop[chain,1];
+(*compute middle price*)
+middlePrice=(ToExpression[chain01[[All,4]]]+ToExpression[chain01[[All,5]]])/2;
+(*compute implied volatility*)
+ivf =Table[FinancialDerivative[{"European","Put"}, {"StrikePrice"-> chain01[[i,6]], "Expiration"->chain01[[i,8]],"Value"->middlePrice[[i]]},  {"InterestRate"-> interestRate,"CurrentPrice"-> currentPrice, "Dividend"->dividend}, "ImpliedVolatility"],{i,1,Length[middlePrice]}];
+(*use teo implid vol to compute greeks*)
+greeks =Table[ FinancialDerivative[{"European","Put"}, {"StrikePrice"-> chain01[[i,6]], "Expiration"->chain01[[i,8]]},  {"InterestRate"-> interestRate, "Volatility" -> ivf[[i]], "CurrentPrice"-> currentPrice, "Dividend"->dividend}, {"Value", "Greeks"}],{i,1,Length[ivf]}];
+(*extract greeks values*)
+teoPrice = Table[greeks[[i,1]],{i,1,Length[ivf]}];
+teoDelta = Table[greeks[[i,2]][[1]][[2]],{i,1,Length[ivf]}];
+teoGamma = Table[greeks[[i,2]][[2]][[2]],{i,1,Length[ivf]}];
+teoRho = Table[greeks[[i,2]][[3]][[2]],{i,1,Length[ivf]}];
+teoTheta = Table[greeks[[i,2]][[4]][[2]],{i,1,Length[ivf]}];
+teoVega =Table[greeks[[i,2]][[5]][[2]],{i,1,Length[ivf]}];
+(*build output *)
+chain02=Table[Append[chain01[[i]],ivf[[i]]],{i,1,Length[ivf]}];
+chain03=Table[Append[chain02[[i]],teoPrice[[i]]],{i,1,Length[ivf]}];
+chain04= Table[Append[chain03[[i]],teoDelta[[i]]],{i,1,Length[ivf]}];
+chain05= Table[Append[chain04[[i]],teoGamma[[i]]],{i,1,Length[ivf]}];
+chain06= Table[Append[chain05[[i]],teoRho[[i]]],{i,1,Length[ivf]}];
+chain07= Table[Append[chain06[[i]],teoTheta[[i]]],{i,1,Length[ivf]}];
+chain08= Table[Append[chain07[[i]],teoVega[[i]]],{i,1,Length[ivf]}];
+chainIvf = Prepend[chain08,
+{"LastUpdateTime","Symbol","LastPrice","BestBid1","BestAsk1","Strike","DToExp","DToExpY","ImpVol","TeoValue","Delta","Gamma","Rho","Theta","Vega"}
+];
+Return[chainIvf];
+];
+
+
+t3ECallGreeksChain[chain_,interestRate_,currentPrice_,dividend_]:=Module[
+{chain01,chain02,chain03,chain04,chain05,chain06,chain07,chain08,chainIvf},
+(*remove column name*)
+chain01=Drop[chain,1];
+(*compute middle price*)
+middlePrice=(ToExpression[chain01[[All,4]]]+ToExpression[chain01[[All,5]]])/2;
+(*compute implied volatility*)
+ivf =Table[FinancialDerivative[{"European","Call"}, {"StrikePrice"-> chain01[[i,6]], "Expiration"->chain01[[i,8]],"Value"->middlePrice[[i]]},  {"InterestRate"-> interestRate,"CurrentPrice"-> currentPrice, "Dividend"->dividend}, "ImpliedVolatility"],{i,1,Length[middlePrice]}];
+(*use teo implid vol to compute greeks*)
+greeks =Table[ FinancialDerivative[{"European","Call"}, {"StrikePrice"-> chain01[[i,6]], "Expiration"->chain01[[i,8]]},  {"InterestRate"-> interestRate, "Volatility" -> ivf[[i]], "CurrentPrice"-> currentPrice, "Dividend"->dividend}, {"Value", "Greeks"}],{i,1,Length[ivf]}];
+(*extract greeks values*)
+teoPrice = Table[greeks[[i,1]],{i,1,Length[ivf]}];
+teoDelta = Table[greeks[[i,2]][[1]][[2]],{i,1,Length[ivf]}];
+teoGamma = Table[greeks[[i,2]][[2]][[2]],{i,1,Length[ivf]}];
+teoRho = Table[greeks[[i,2]][[3]][[2]],{i,1,Length[ivf]}];
+teoTheta = Table[greeks[[i,2]][[4]][[2]],{i,1,Length[ivf]}];
+teoVega =Table[greeks[[i,2]][[5]][[2]],{i,1,Length[ivf]}];
+(*build output *)
+chain02=Table[Append[chain01[[i]],ivf[[i]]],{i,1,Length[ivf]}];
+chain03=Table[Append[chain02[[i]],teoPrice[[i]]],{i,1,Length[ivf]}];
+chain04= Table[Append[chain03[[i]],teoDelta[[i]]],{i,1,Length[ivf]}];
+chain05= Table[Append[chain04[[i]],teoGamma[[i]]],{i,1,Length[ivf]}];
+chain06= Table[Append[chain05[[i]],teoRho[[i]]],{i,1,Length[ivf]}];
+chain07= Table[Append[chain06[[i]],teoTheta[[i]]],{i,1,Length[ivf]}];
+chain08= Table[Append[chain07[[i]],teoVega[[i]]],{i,1,Length[ivf]}];
+chainIvf = Prepend[chain08,
+{"LastUpdateTime","Symbol","LastPrice","BestBid1","BestAsk1","Strike","DToExp","DToExpY","ImpVol","TeoValue","Delta","Gamma","Rho","Theta","Vega"}
+];
+Return[chainIvf];
 ];
 
 
